b756ea21f62280b7fcaa18b375a931d394b08ddd
jin521 <jinlc220@126.com>
2016-07-05 23:31:28 +1000

    oceans_app

---
+++ jin_qiu/week_05/oceans/oceans_app/db/seeds.rb
@@ -0,0 +1,7 @@
+Ocean.destroy_all
+
+Ocean.create :name => 'Pacific', :area => 168723000, :depth => 4280
+Ocean.create :name => 'Atlantic', :area => 85133000, :depth => 3339
+Ocean.create :name => 'Indian', :area => 70560000, :depth => 8047
+Ocean.create :name => 'Southern', :area => 21960000, :depth => 4000
+Ocean.create :name => 'Arctic', :area => 15558000, :depth => 3406
